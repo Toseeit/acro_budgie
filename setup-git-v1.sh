d8e63bfb13c451e2b4584ff0bb9991a4d3d13cff
John Dunlop <dunjohn@live.com.au>
2020-05-03 04:44:38 +0800

    First commit

---
+++ setup-git-v1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "John Dunlop"
+git config --global user.email "dunjohn@live.com.au"
+sudo git config --system core.editor sublime
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
